6da46c7f0089381fc5dc5f314bb0bd96de475e01
Brian McClain <brianmmcclain@gmail.com>
2018-10-01 17:05:24 -0400

    Updated to simple Hello response

--- app.go
+++ app.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func handlePost(rw http.ResponseWriter, req *http.Request) {
-	// Say Hello!
-	fmt.Fprintf(rw, "%s", "Hello from Buildpacks!!")
-}
-
-func main() {
-	log.Print("Starting server on port 8080...")
-	http.HandleFunc("/", handlePost)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func HelloBuildpacks(in string) string {
+	return "Hello from Buildpacks!"
 }
