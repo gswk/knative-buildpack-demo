81d7df2ed543dc62de9ab4d523f1e17d012b7f88
Brian McClain <brianmmcclain@gmail.com>
2018-10-08 14:19:41 -0400

    Update node example

--- hello.js
+++ hello.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  console.log('Hello world received a request.');
-
-  var target = process.env.TARGET || 'NOT SPECIFIED';
-  res.send('Hello world: ' + target);
+    res.send("Hello from Buildpacks!");
 });
 
 var port = 8080;
 app.listen(port, function () {
-  console.log('Hello world listening on port',  port);
+    console.log('Hello world listening on port',  port);
 });
